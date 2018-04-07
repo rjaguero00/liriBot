25cb84974b466f6cb6a2670ab18a1a9b7096621f
Roberto aguero <rjaguero00@Robertos-MacBook-Air.local>
2018-04-06 18:48:18 -0700

    installs

--- liri.js
+++ liri.js
@@ -1,4 +1,12 @@
 require("dotenv").config();
 
+var keys = require('./keys.js');
+
+var request = require('request');
+var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
+var fs = require("fs");
+
+//Keys for spotify and twitter
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
